f03ad27eaf38b33d198d45c8e59e35d7d8a3f416
Abhyast-Dev <65902037+Abhyast-Dev@users.noreply.github.com>
2024-06-04 20:51:02 +0530

    Update My.py

--- My.py
+++ My.py
@@ -39,44 +39,58 @@ def Plan():
         sleep = st.number_input("Sleep: No. of hours",min_value=0)
         impsleep = st.slider("Choose level of importance on a scale of 1 to 10", 0,10,1,key='sleep')
         submitted = st.form_submit_button("Submit")
-        d = {'Date': [dt],'TV':[tv],'TV_imp' :[imptv],'Guitar':[Guitar], 'Guitar_imp': [impguitar],'Music':[music],'Music_imp': [impmusic], 'Playing': [playing], 'Playing_imp' : [impplaying],  'Homework' : [homework],'Homework_imp': [imphomework],'Selfstudy': [selfstudy], 'Selfstudy_imp' : [impselfstudy],'Sleep':[sleep],'Sleep_imp':[impsleep]} 
+        d = {'Date': [dt],'TV':[tv],'TV_imp' :[imptv],'Guitar':[Guitar], 'Guitar_imp': [impguitar],'Music':[music],'Music_imp': [impmusic], 'Playing': [playing], 'Playing_imp' : [impplaying],  'Homework' : [homework],'Homework_imp': [imphomework],'Selfstudy': [selfstudy], 'Selfstudy_imp' : [impselfstudy],'Sleep':[sleep],'Sleep_imp':[impsleep],'Achieved?':[False]} 
         myact=pd.DataFrame(d)     
         
         if submitted:
-                writetofile(myact)
+                myact.to_csv("activities.csv",mode='a',header=False,index =False)
                 
                 st.markdown('Submitted responses:')
 def ToDo():
     df= pd.read_csv("todo.csv")
     tdate= datetime.now().date()-timedelta(days=1)
     mydf = df['Date'] == str(tdate)
-    rslt_df = df[df['Date'] == str(tdate)]
+    rslt_df = df[mydf]
     st.write(":blue[The victory of success is half won when one gains the habit of setting goals and achieving them.]")
-   
+    desc=None
     if not(df.empty):
-     
+        
+        on = st.toggle("Hi Abeer 👦🏻! Could you achieve your yesterday's plan?")
+        if(on==True):
+            num = st.slider("Achieved?,Choose from a scale of 0 to 5. 0 means -> Not at all, 1-> 25%, 2-> 50%, 3->75%, 4-> All Done  ",min_value=0, max_value=4)
+            ach = True      
+            if num==0:
+                desc = " Not at all"
+            elif num == 1:
+                desc = "25%"
+            elif num ==2:
+                desc = '50%'
+            elif num == 3:
+                desc = '75%'
+            else:
+                desc = 'All Done'
+            st.balloons() 
+        else:
+            ach = False                
+            st.toast('Imagine!',icon="🏅")
+            time.sleep(.5)
+            st.toast('Believe!',icon="🤩")
+            time.sleep(.5)
+            st.toast('Achieve!', icon='🎉')
+        
+            
         with st.form('ytodo',clear_on_submit=False):
             st.dataframe(rslt_df,hide_index=True)
             submitted = st.form_submit_button("Submit")
-            on = st.toggle("Could you achieve?")
-            rem=st.text_area("Remarks",key="Remarks")
             
-            if on == True:
-                ach = True                
-                st.balloons()
-            else:
-                ach = False                
-                st.toast('Imagine!',icon="🏅")
-                time.sleep(.5)
-                st.toast('Believe!',icon="🤩")
-                time.sleep(.5)
-                st.toast('Achieve!', icon='🎉')
+            rem=st.text_area("Remarks",key="Remarks")
+                   
             df.loc[mydf, 'Achieved'] = ach
+            df.loc[mydf, 'Desc'] = desc
             df.loc[mydf, 'Remarks'] = rem
             if submitted:
-                    
-                    df.to_csv("todo.csv",mode='a', header=False,index =False)
-                    st.markdown('Submitted response')
+                    df.to_csv("todo.csv",index =False)
+                    st.success('Submitted response')
         
 
                     
@@ -84,25 +98,16 @@ def ToDo():
     with st.form('mytodo',clear_on_submit=True):
         dt = st.date_input("Plan your day for :", datetime.now().date()+timedelta(days=1),format="YYYY/MM/DD")
         todo = st.text_area("Plan for tomorrow",key='todo') 
-        num = st.slider("Achieved?,Choose from a scale of 0 to 5. 0 means -> Not at all, 1-> 25%, 2-> 50%, 3->75%, 4-> All Done  ",min_value=0, max_value=4)
+        
         submitted = st.form_submit_button("Submit")
-        if num==0:
-            desc = " Not at all"
-        elif num == 1:
-            desc = "25%"
-        elif num ==2:
-            desc = '50%'
-        elif num == 3:
-            desc = '75%'
-        else:
-            desc = 'All Done'
-        d = {'Date': [dt],'Todo':[todo],'Achieved':num,'Desc': desc} 
+        
+        d = {'Date': [dt],'Todo':[todo],'Achieved':None,'Remarks':None,'Desc': None} 
         myact=pd.DataFrame(d)       
 
         if submitted:
                 myact.to_csv("todo.csv",mode='a', index=False, header=False)
                 
-                st.markdown('Submitted response')
+                st.success('Response Saved')
     
 
     fig = px.pie(values = df['Achieved'],hole = 0.5)
@@ -121,7 +126,7 @@ def achieve():
     st.write(":blue[Time management is not about managing TIME. It's about managing priorities]")
     df = openfile()
     tdate= datetime.now().date() 
-    myloc=    df['Date'] == str(tdate)
+    myloc = df['Date'] == str(tdate)
     rslt_df = df[myloc]
     st.dataframe(rslt_df,hide_index=True)
     Do={}
@@ -133,55 +138,52 @@ def achieve():
         field = str(i)
         
         if field.find('imp')!=-1:
-            reslt = rslt_df.loc[myloc,i]
-            res = reslt._get_value(0,i)
-            st.write("Imp is not present")
-                       
-            if (res>=8) & (res<=10):
+            res = rslt_df[i]
+                                        
+            if (res.item()>=8) & (res.item()<=10):
                 Do[i]= x
-            elif (res>=5) & (res<=7):
+            elif (res.item()>=5) & (res.item() <=7):
                 Later[i]=x
             else:
                 Ignore[i] =x
-        else:
-            st.write("No value in res")
+        
     with st.form("valueupdate"):
 
         st.text("Could you achieve your plan?")
 
-        container = st.container(border=True)
+        
         status=False
         
-        with container:
-            st.caption("Absolutely necessary, ")
-            for i in Do:
-                st.success( str(i)+" :->"+str(Do[i])+" hours")
-            st.caption("Leisure time, can be done later")
-            for i in Later:
-                st.warning( str(i)+" :->"+str(Later[i])+" hours")
-            st.caption("Can be ignored")
-            for i in Ignore:
-                st.info( str(i)+" :->"+str(Ignore[i])+" hours")
-            on = st.toggle("Everything went as planned")
-    
-            if on:
-                st.balloons()
-                status=True
-            else:
-                status = False
-                st.toast('Imagine!',icon="🏅")
-                time.sleep(.5)
-                st.toast('Believe!',icon="🤩")
-                time.sleep(.5)
-                st.toast('Achieve!', icon='🎉')
+        
+        st.info("Absolutely necessary, ")
+        for i in Do:
+            st.success( str(i)+" :->"+str(Do[i])+" hours")
+        st.info("Leisure time, can be done later")
+        for i in Later:
+            st.warning( str(i)+" :->"+str(Later[i])+" hours")
+        st.info("Can be ignored")
+        for i in Ignore:
+            st.error( str(i)+" :->"+str(Ignore[i])+" hours")
+        on = st.toggle("Everything went as planned")
+
+        if on:
+            st.balloons()
+            status=True
+        else:
+            status = False
+            st.toast('Imagine!',icon="🏅")
+            time.sleep(.5)
+            st.toast('Believe!',icon="🤩")
+            time.sleep(.5)
+            st.toast('Achieve!', icon='🎉')
 
         submitted =  st.form_submit_button("Click to update status")
-        updated = df['Date'] == tdate
-        
-        df.loc[updated, 'Achieved?'] = status
+        df.loc[myloc, 'Achieved?'] = status
+        st.write(df)
 
         if submitted:
-            writetofile(df)
+            st.success("Status Saved!")
+            df.to_csv("activities.csv",index =False)
 
     
 def share():
@@ -212,7 +214,7 @@ def share():
         
                 
         if feelsubmitted:
-            
+            st.success("Status Saved!")            
             feeldf.to_csv("expressself.csv",mode='a',header=False, index=False)
    
           
