45f56530aefe54ce39094a77e48bab725515d2f6
Abhyast-Dev <65902037+Abhyast-Dev@users.noreply.github.com>
2025-01-22 20:26:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ elif choice == "***Run up to Board Exams***":
     tab1,tab2 = st.tabs(["Today's Target","Complete plan"])
     with tab1:
         st.caption(" My dear Abeer")
-        st.caption("Setting targets is the key to unlocking your true potential. Scoring well is not just about the marks but about giving your best with discipline, focus, and effort. When you perform to your full potential, you leave no room for regrets—only the satisfaction of knowing you gave it your all.")
+        st.caption(":blue[Setting targets is the key to unlocking your true potential. Scoring well is not just about the marks but about giving your best with discipline, focus, and effort. When you perform to your full potential, you leave no room for regrets—only the satisfaction of knowing you gave it your all.]")
         st.write("Today's Target")
     with tab2:
         st.dataframe(df)
